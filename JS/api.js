c05279213256a55a22a502a14a7ed920d96d09fd
ludenhermoncorrales <ludencorrales@gmal.com>
2025-12-04 11:19:55 -0300

    add GET and POST of thought

--- JS/api.js
+++ JS/api.js
@@ -1,14 +1,30 @@
 const api = {
-async searchThought (){
-    try {
-        const respose = await fetch('http://localhost:3000/pensamentos2');
-        return await respose.json();
-        
-    } catch (error) {
-        alert('Erro ao buscar pensamentos');
-        throw error;
+    async searchThought() {
+        try {
+            const respose = await fetch('http://localhost:3000/pensamentos');
+            return await respose.json();
+
+        } catch (error) {
+            alert('Erro ao buscar pensamentos');
+            throw error;
+        }
+    },
+
+    async saveThought(thought) {
+        try {
+            // Criar um mapa explicativo e estudar a fundo a estrutura aplicada nesse POST
+            const respose = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(thought)
+
+            })
+            return await respose.json()
+        } catch (error) {
+            alert('Erro ao buscar pensamentos');
+            throw error;
+        }
     }
-}
 }
 
 export default api;
